437c397dd797e770aede84af54a58a50679b447b
Rupesh155 <rudhg35@gmail.com>
2026-07-27 16:00:48 +0530

    React basic

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StateExample from './class_01/StateExample'
+
+const App = () => {
+  return (
+    <div>
+      <StateExample/>
+    </div>
+  )
+}
+
+export default App
