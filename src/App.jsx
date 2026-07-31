ccf1585a79541b48518578cb1a00ccac808ee9ab
Rupesh155 <rudhg35@gmail.com>
2026-07-31 09:45:13 +0530

    dynamic routinggg

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import NavBar from './class_03/NavBar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './class_03/Home'
 import About from './class_03/About'
+import ProductList from './class_03/ProductList'
+import ProductsD from './class_03/ProductsD'
 // import StateExample from './class_01/StateExample'
 // https://github.com/Rupesh155
 
@@ -13,11 +15,17 @@ const App = () => {
       {/* <StateExample/> */}
        {/* <UseEffectDemo/> */}
        <NavBar/>
+       {/* http://localhost:5173/p/1 */}
        <Routes>
-        <Route  path='/'   element={<Home/>}/>
-        <Route  path='/about'   element={<About/>}/>
+        <Route   path='/'  element={<Home/>}/>
+        <Route   path='/about'  element={<About/>}/>
+        <Route   path='/list'  element={<ProductList/>}/>
+        <Route   path='/p/:id'  element={<ProductsD/>}/>
+
+
 
        </Routes>
+
     </div>
   )
 }
@@ -26,3 +34,6 @@ export default App
 
 
 // localhost:5173/
+
+
+
--- src/class_03/NavBar.jsx
+++ src/class_03/NavBar.jsx
@@ -4,13 +4,17 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
   return (
     <div className='nav'>
-        <Link to={'/'}> 
+        <Link to={"/"}>   
+   
         <li>Home</li>
         </Link>
+       
         <Link to={'/about'}>  
         <li>About</li>
         </Link>
-        <li>Contact Us</li>
+        <li>list</li>
+
+        {/* https://www.reddit.com/r/cats/ */}
 
 
     </div>
